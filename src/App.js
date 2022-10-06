41ef4c8b39801efb2152983239d7816cef36411f
Eliot <eliot.meurillon@gmail.com>
2022-10-06 16:42:16 +0200

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Test 2!</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline grid h-screen place-items-center">
+      Test 2!
+    </h1>
+  );
 }
 
 export default App;
