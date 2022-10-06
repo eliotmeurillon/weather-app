2999153998d3769783ea0e465e8244962bdfee4e
Eliot <eliot.meurillon@gmail.com>
2022-10-06 17:39:10 +0200

    qs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div class="flex items-center justify-center h-screen">
+    <div class="flex items-center justify-center h-screen bg-gray-400">
       <div class="max-w-md bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
         <div class="md:flex">
           <div class="md:shrink-0">
