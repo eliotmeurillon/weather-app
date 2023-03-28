b9f827b2eea8918989a2c87e51caa6d3a7c7c3db
Eliot <eliot.meurillon@gmail.com>
2023-03-29 01:14:49 +0200

    change for one loading

--- src/App.js
+++ src/App.js
@@ -71,16 +71,12 @@ function App() {
       <div class=" bg-[#C9E5FF]/90 backdrop-blur-lg rounded-xl shadow-xl overflow-hidden">
         <div class="flex flex-col  justify-center p-5 font-Jakarta text-[#658CAF] font-semibold">
           {state === "loading" ? (
-            <p>Loading...</p>
+            <p class="text-2xl">Loading...</p>
           ) : (
             <div>
               <div class="flex flex-row justify-between items-center">
                 <div class="text-2xl bg-white/20 rounded-md shadow-sm p-2">
-                  {state === "loading" ? (
-                    <p>Loading...</p>
-                  ) : (
-                    <p>{firstWordCityName}</p>
-                  )}
+                  <p>{firstWordCityName}</p>
                 </div>
                 <div class="text-2xl">
                   <p>
@@ -93,52 +89,33 @@ function App() {
                 </div>
               </div>
               <div class="flex flex-row items-center">
-                {state === "loading" ? (
-                  <p class="text-2xl">Loading...</p>
-                ) : (
-                  <img class="w-40" alt="weather-icon" src={logo} />
-                )}
+                <img class="w-40" alt="weather-icon" src={logo} />
+
                 <div class="flex flex-col">
-                  {state === "loading" ? (
-                    <p class="text-7xl">Loading...</p>
-                  ) : (
-                    <h1 class="text-transparent text-7xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 font-bold">
-                      {weatherRound}°C
-                    </h1>
-                  )}
-                  {state === "loading" ? (
-                    <p>Loading...</p>
-                  ) : (
-                    <p class="text-lg ml-2">
-                      {weather?.weather?.[0].description}
-                    </p>
-                  )}
+                  <h1 class="text-transparent text-7xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 font-bold">
+                    {weatherRound}°C
+                  </h1>
+
+                  <p class="text-lg ml-2">
+                    {weather?.weather?.[0].description}
+                  </p>
                 </div>
               </div>
               <div class="grid grid-cols-3 gap-2 justify-around text-base">
                 <div class="flex flex-col items-center bg-white/20 rounded-md shadow-sm p-1">
                   <img class="w-10" alt="humidity-icon" src={humidityLogo} />
-                  {state === "loading" ? (
-                    <p>Loading...</p>
-                  ) : (
-                    <p>{weather?.main?.humidity} %</p>
-                  )}
+
+                  <p>{weather?.main?.humidity} %</p>
                 </div>
                 <div class="flex flex-col items-center bg-white/20 rounded-md shadow-sm p-1">
                   <img class="w-10" alt="humidity-icon" src={windLogo} />
-                  {state === "loading" ? (
-                    <p>Loading...</p>
-                  ) : (
-                    <p>{weather?.wind?.speed} m/s</p>
-                  )}
+
+                  <p>{weather?.wind?.speed} m/s</p>
                 </div>
                 <div class="flex flex-col items-center bg-white/20 rounded-md shadow-sm p-1">
                   <img class="w-10" alt="humidity-icon" src={pressureLogo} />
-                  {state === "loading" ? (
-                    <p>Loading...</p>
-                  ) : (
-                    <p>{weather?.main?.pressure} hPa</p>
-                  )}
+
+                  <p>{weather?.main?.pressure} hPa</p>
                 </div>
               </div>
             </div>
